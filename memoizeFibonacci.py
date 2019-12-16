c6af5ff578aa6f622da7dbc87d0a68a0f86668a9
taha1337 <38756701+taha1337@users.noreply.github.com>
2019-12-16 00:27:20 -0500

    Add files via upload
    Initial

---
+++ memoizeFibonacci.py
@@ -0,0 +1,20 @@
+import array as arr
+
+f = arr.array('i',[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,])
+
+def fib_mem_init():
+  f = arr.array('i',[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,])
+
+#template<typename T>
+def fib_mem(n):
+  if f[n] == -1:  # if f[n] is default, compute
+    if (n == 0) or (n == 1): # base case
+      return 1
+    else:
+      f[n] = fib_mem(n-1) + fib_mem(n-2)
+      return f[n]    
+  else:
+    return f[n]
+
+fib_mem_init()
+print(fib_mem(25))
